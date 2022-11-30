ddb5c2ce9b95c66699704c2631ce01774f80b171
Smaidziunas <sdamarcius@gmail.com>
2022-11-30 08:35:40 +0200

    08_r_styling_module_css

---
+++ src/components/card/Card.jsx
@@ -0,0 +1,13 @@
+function Card(props) {
+  return (
+    <div className='card'>
+      <h2>James Bond</h2>
+      <h3>Title: 007</h3>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et quas
+        corporis consequuntur veritatis facilis sequi ut veniam ab neque cum.
+      </p>
+    </div>
+  );
+}
+export default Card;
